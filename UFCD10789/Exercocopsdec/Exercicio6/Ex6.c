c88fcae26053d0d416df91b4c7c259e5701f26e6
Rafael Santos <rafael.santos.t0126455@edu.atec.pt>
2025-04-22 15:17:56 +0100

    Exercicios de C

--- UFCD10789/Exercocopsdec/Exercicio6/Ex6.c
+++ UFCD10789/Exercocopsdec/Exercicio6/Ex6.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
 
+int main(){
+    //Perguntar o nome do cliente e o valor da compra do mesmo
+    //Se a compra for de 200.00€ então deverá receber 10% de desconto
+    //15% de desconto se for maior que 200€ e menor ou igual a 500.00€
+    //20% se o valor da compra for superior a 500.00€
+    //No final mostre o nome do cliente, o valor da compra, o percentagem do desconto e o valor total a pagar.
+
+    int nome;
+    int valor;
+    int ;
+
+    printf("Introduza o seu nome: ");
+    scanf("%d", nome);
+
+    printf("Introduza o valor da sua compra: ");
+    scanf("%d", valor);
+
+    desconto
+
+    if(valor == 200){
+        printf("")
+    }
+
+}
