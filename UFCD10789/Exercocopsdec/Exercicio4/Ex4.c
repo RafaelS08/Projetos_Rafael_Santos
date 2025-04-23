5e364c6a18cbb2efb4f1df5222a76c04f15855dc
Rafael Santos <rafael.santos.t0126455@edu.atec.pt>
2025-04-23 23:52:44 +0100

    Correção de Erros dos Exercicios de C

--- UFCD10789/Exercocopsdec/Exercicio4/Ex4.c
+++ UFCD10789/Exercocopsdec/Exercicio4/Ex4.c
@@ -14,16 +14,13 @@ int main(){
     desconto = saldo - cheque;
 
     if(cheque <= saldo){
-<<<<<<< HEAD
-        printf("Foi poss�vel realizar o desconto no cheque, ficou com o valor de %d como o seu saldo atual", desconto);
+        printf("Foi possovel realizar o desconto no cheque, ficou com o valor de %d como o seu saldo atual", desconto);
     }
     else if(cheque > saldo){
-        printf("N�o � poss�vel realizar o desconto no cheque pois o valor do cheque � superior ao seu saldo atual");
-=======
+        printf("Nao e possivel realizar o desconto no cheque pois o valor do cheque e superior ao seu saldo atual");
         printf("Foi possível realizar o desconto no cheque, ficou com o valor de %d como o seu saldo atual", desconto);
     }
     else if(cheque > saldo){
         printf("Não é possível realizar o desconto no cheque pois o valor do cheque é superior ao seu saldo atual");
->>>>>>> 97d9459205a5db835f8d1a58ce9012c670e114ea
     }
 }
--- UFCD10789/Exercocopsdec/Prova de Avaliação/ProvadeAvaliação.c
+++ UFCD10789/Exercocopsdec/Prova de Avaliação/ProvadeAvaliação.c
@@ -7,14 +7,15 @@ int main() {
     int i;
     int acimadamedia = 0;
 
-    for (i = 0; i < 10; i++){
-        printf("Digite a nota do aluno %d (0 a 20): ", i + 1);
+    for (i = 0; i < 10; i++) {
+        printf("Insira a nota do aluno %d (0 a 20): ", i + 1);
         scanf("%f", &notas[i]);
-        if (notas[i] < 0 || notas[i] > 20) {
-            printf("Nota inválida! Digite um valor entre 0 e 20.\n");
-            }
+
+        while (notas[i] < 0 || notas[i] > 20) {
+            printf("Nota inválida! Insira um valor entre 0 e 20: ");
+            scanf("%f", &notas[i]);
         }
-    while (notas[i] < 0 || notas[i] > 20){
+
         soma += notas[i];
     }
 
@@ -22,12 +23,12 @@ int main() {
 
     for (i = 0; i < 10; i++) {
         if (notas[i] >= media) {
-            acimaMedia++;
+            acimadamedia++;
         }
     }
 
     printf("\nMédia da turma: %.2f\n", media);
-    printf("Número de alunos com nota igual ou acima da média: %d\n", acimaMedia);
+    printf("Número de alunos com nota igual ou acima da média: %d\n", acimadamedia);
 
     return 0;
 }
